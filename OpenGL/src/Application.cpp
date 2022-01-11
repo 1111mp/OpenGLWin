98c9bb3cd17e25d16ad3cede21061f5f5f337f54
imp <say_1mp@163.com>
2022-01-11 21:55:46 +0800

    feat: 3D

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -61,36 +61,36 @@ int main(void)
     //  -0.5f,  0.5f, 1.0f, 1.0f, 1.0f  // 3
     //};
     float positions[] = {
-      -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-       0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-       0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+      -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // 0
+       0.5f, -0.5f, -0.5f,  1.0f, 0.0f, // 1
+       0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // 2
        //0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-      -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+      -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // 3
       //-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 
-      -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-       0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-       0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+      -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // 4
+       0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // 5
+       0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // 6
        //0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-      -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+      -0.5f,  0.5f,  0.5f,  0.0f, 1.0f, // 7
       //-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
 
-      -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-      //-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-      -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+      -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // 8
+      -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // 9
+      -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // 10
       //-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
       //-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
       //-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
 
-       0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+       0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // 11
        //0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-       0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+       0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // 12
        //0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-       0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+       0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // 13
        //0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
 
       //-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-       0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+       0.5f, -0.5f, -0.5f,  1.0f, 1.0f, // 14
        //0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
        //0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
       //-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
@@ -100,20 +100,25 @@ int main(void)
        //0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
        //0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
        //0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-      -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+      -0.5f,  0.5f,  0.5f,  0.0f, 0.0f, // 15
       //-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
 
     unsigned int indices[] = {
-      0, 1, 2,
-      0, 2, 3
+      0,   1,  2,  2,  3,  0,
+      4,   5,  6,  6,  7,  4,
+      8,   9, 10, 10,  4,  8,
+      11,  2, 12, 12, 13, 11,
+      10, 14,  5,  5,  4, 10,
+       3,  2, 11, 11, 15,  3
     };
 
-    glm::mat4 model = glm::rotate(glm::mat4(1.0f), glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
+    glEnable(GL_DEPTH_TEST);
+
+    //glm::mat4 trans;
     // Õ∏ ”Õ∂”∞
-    glm::mat4 proj = glm::perspective(45.0f, (640.0f / 480.0f), 0.1f, 100.0f);
-    glm::mat4 trans = proj * view * model;
+    
+    //trans = glm::rotate(trans, glm::radians((GLfloat)glfwGetTime() * 50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 
     //trans = glm::scale(trans, glm::vec3(0.5f, 0.5f, 0.5f));
 
@@ -128,12 +133,12 @@ int main(void)
     layout.Push<float>(2);
     va.AddBuffer(vb, layout);
 
-    IndexBuffer ib(indices, 6);
+    IndexBuffer ib(indices, 36);
 
     Shader shader("resource/shaders/Basic.shader");
     shader.Bind();
     //shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
-    shader.SetUniformMat4f("u_Transform", trans);
+    //shader.SetUniformMat4f("u_Transform", trans);
 
     //float r = 0.0f;
     //float increment = 0.05f;
@@ -156,11 +161,17 @@ int main(void)
     while (!glfwWindowShouldClose(window))
     {
       /* Render here */
-      //glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+      glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
       renderer.Clear();
 
       shader.Bind();
+      glm::mat4 trans = glm::perspective(45.0f, (640.0f / 480.0f), 0.1f, 100.0f);
+      trans = glm::translate(trans, glm::vec3(0.0f, 0.0f, -3.0f));
+      trans = glm::rotate(trans, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
       //shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+      trans = glm::rotate(trans, glm::radians((GLfloat)glfwGetTime() * 50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+      shader.SetUniformMat4f("u_Transform", trans);
 
       renderer.Draw(va, ib);
 
